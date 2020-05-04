6504a812c00526eb889033505005025e3a1e87c4
Nancy Eckenthal <nancy.eckenthal@gmail.com>
2020-05-03 22:20:34 -0400

    added content and styling tricks

--- index.js
+++ index.js
@@ -1,25 +1,63 @@
-function copyToClipboard() {
-    /* create a fake input element */
-    const input = document.createElement('input')
-    input.value = "nancy.eckenthal@gmail.com"
+const copyToClipboard = () => {
+  /* create a fake input element */
+  const input = document.createElement("input");
+  input.value = "nancy.eckenthal@gmail.com";
 
-    document.body.appendChild(input)
+  document.body.appendChild(input);
 
-    /* Select the text field */
-    input.select();
-    input.setSelectionRange(0, 99999); /*For mobile devices*/
-  
-    /* Copy the text inside the text field */
-    document.execCommand("copy");
+  /* Select the text field */
+  input.select();
+  input.setSelectionRange(0, 99999); /*For mobile devices*/
 
-    document.body.removeChild(input)
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  document.body.removeChild(input);
+};
+
+const header = document.getElementById("sticky-header");
+
+const getDistanceFromTop = () => header.offsetTop;
+
+const stickPoint = getDistanceFromTop();
+
+const addStickyHeader = () => {
+  const distance = stickPoint - window.pageYOffset;
+  const offset = window.pageYOffset;
+  if (distance <= 0) {
+    header.classList.add("sticky");
+  } else if (offset <= stickPoint) {
+    header.classList.remove("sticky");
   }
+};
 
+const topButton = document.getElementById("top-button");
 
-  document.querySelector("img.email").addEventListener('click', (e) => {
-      copyToClipboard()
-      const snackbar = document.getElementById("snackbar")
-      snackbar.className = "show"
-      setTimeout(() => snackbar.className = snackbar.className.replace("show", ""), 3000);
+const userDistanceFromTop =
+  window.pageYOffset !== undefined
+    ? window.pageYOffset
+    : document.body.scrollTop;
 
-  })
+const showBackToTopButton = () => {
+  if (window.pageYOffset <= 10) {
+    topButton.className = topButton.className.replace("show", "");
+  } else {
+    console.log("here");
+    topButton.className = "show";
+  }
+};
+
+document.querySelector("img.email").addEventListener("click", (e) => {
+  copyToClipboard();
+  const snackbar = document.getElementById("snackbar");
+  snackbar.className = "show";
+  setTimeout(
+    () => (snackbar.className = snackbar.className.replace("show", "")),
+    3000
+  );
+});
+
+window.onscroll = () => {
+  addStickyHeader();
+  showBackToTopButton();
+};
